7a2423600aae61160b0cc06c0279d022d42e1904
Anton Shubin <2spy4x@gmail.com>
2017-03-13 14:45:35 +0300

    add logs to heroku.sh file

--- heroku.sh
+++ heroku.sh
@@ -1,4 +1,12 @@
+echo "starting heroku.sh script"
 ls -la
+echo "ls - completed"
+cat gulpfile.js
+echo "cat gulpfile.js - completed"
 yarn global add gulp
+echo "yarn global add gulp - completed"
 gulp generate-secrets
+echo "gulp generate-secrets - completed"
 cat build/config/secrets.json
+echo "cat build/config/secrets.json - completed"
+echo "finishing heroku.sh script"
